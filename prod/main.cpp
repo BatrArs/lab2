28aa4192a1d2c2cdc0f5142b7c4708b400664ca0
BatrArs <144597946+BatrArs@users.noreply.github.com>
2023-11-30 12:53:04 +0300

    Add files via upload

---
+++ prod/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cctype>
+#include "modAlphaCipher.h"
+#include <locale>
+#include <codecvt>
+#include <typeinfo>
+using namespace std;
+void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+{
+    try {
+        wstring cipherText;
+        wstring decryptedText;
+        modAlphaCipher cipher(key);
+        cipherText = cipher.encrypt(Text);
+        if (destructCipherText)
+            cipherText.front() = towlower(cipherText.front());
+        decryptedText = cipher.decrypt(cipherText);
+        wcout<<L"key="<<key<<endl;
+        wcout<<Text<<endl;
+        wcout<<cipherText<<endl;
+        wcout<<decryptedText<<endl;
+    } catch (const cipher_error & e) {
+        wcerr<<"Error: "<<e.what()<<endl;
+    }
+
+}
+
+int main()
+{
+    check(L"ars",L"origin");
+    check(L"arseniy",L"c1imber");
+    check(L"sen4ik",L"");
+    check(L"sen4ik",L" PGU");
+    check(L"g o l f",L"PGU");
+    check(L"585858",L"boooom");
+}
